4eae34fa85d3aa32bb539a6ed76d659200960888
Racemic3654 <koshtinipun2002@gmail.com>
2025-04-09 01:40:28 +0530

    background changes

--- AngularB/src/app/app.routes.ts
+++ AngularB/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Component/home/home.component';
+import { ContainerComponent } from './Component/container/container.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:HomeComponent
+    },
+    {
+        path:'Container',
+        component:ContainerComponent
+    }
+    
+];
